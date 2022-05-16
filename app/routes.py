4a284e26e482ccf11aa5847f9ee98b5f44c1a575
Josh <jtclauson@gmail.com>
2022-05-16 22:28:42 +0900

    update

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,30 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session
 from app import app
 from format import Format
 from format import Formatted
-import logging
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # logging.basicConfig(filename="std.log", 
-					# format='%(asctime)s %(message)s', 
-					# filemode='w') 
-    # logger=logging.getLogger() 
-
-    #Now we are going to Set the threshold of logger to DEBUG 
-    # logger.setLevel(logging.DEBUG) 
-
-    #some messages to test
-    # logger.debug("here01") 
 
     if request.method == 'POST':
         # text = request.form["text"]
-        input = request.form
-        formatted = Format(input)
-        return redirect(url_for('formatted', testvar="toast"))
+        # input = request.form
+        # formatted = Format(input)
+        testvar = "pancakes"
+        session['testvar'] = testvar
+        return redirect(url_for('formatted'))
         # return redirect(url_for('formatted', text=text))
     return render_template('index.html')
 
 @app.route("/formatted", methods=['GET', 'POST'])
-def formatted(testvar):
+# @app.route('/formatted')
+def formatted(testvar="default"):
+    # testvar = request.args.get('testvar')
+    testvar = session['testvar']
     # testvar="jam"
     # text = request.form.get('text')
     # input = request.form.get('formatted')
     return render_template('formatted.html', testvar=testvar)
+    # return 'welcome %s' % testvar
     # return render_template('formatted.html')
     # return render_template('formatted.html', text=text)
